d5dc6dd2126691e39ba5c82b46ecda1020a46148
madebymak <mr.jmak@gmail.com>
2016-11-08 02:43:41 +0000

    Made additional changes to getRepoContributors function (add too much code

--- download_avatars.js
+++ download_avatars.js
@@ -1,17 +1,14 @@
 var request = require('request');
 //console.log("Welcome to the GitHub Avatar Downloader!");
+//var GITHUB_USER = "madebymak";
+//var GITHUB_TOKEN = "958480b71aa96b2bd710ee40b90c766a8a9e2a38"
+
+//var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+
+//console.log(requestURL);
 
 function getRepoContributors (repoOwner, repoName, cb) {
 
-  request.get("https://api.github.com/repos/jquery/jquery/contributors")
-    .on("error", function (err) {
-      throw err;
-    })
-
-    .on("response", function (response) {
-      console.log("Reponse message:", response.statusMessage);
-      //console.log(cb)
-    })
 
 }
 
