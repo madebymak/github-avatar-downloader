7fa4a0026a3299e326719e143aaa4140f212251f
madebymak <mr.jmak@gmail.com>
2016-11-08 02:13:25 +0000

    Added getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
+//console.log("Welcome to the GitHub Avatar Downloader!");
 
-console.log("Welcome to the GitHub Avatar Downloader!");
+function getRepoContributors (repoOwner, repoName, cb) {
+
+  request.get("https://api.github.com/repos/jquery/jquery/contributors")
+    .on("error", function (err) {
+      throw err;
+    })
+
+    .on("response", function (response) {
+      console.log("Reponse message:", response.statusMessage);
+      //console.log(cb)
+    })
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
