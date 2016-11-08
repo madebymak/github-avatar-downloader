e0de8a5ffe702ff8f1b8e66d530edaaa69535978
madebymak <mr.jmak@gmail.com>
2016-11-08 05:55:19 +0000

    Added downloadImageByURL function to download individiual avatar pics.

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "madebymak";
 var GITHUB_TOKEN = "958480b71aa96b2bd710ee40b90c766a8a9e2a38"
@@ -11,17 +12,33 @@ function getRepoContributors (repoOwner, repoName, cb) {
     url: requestURL,
     headers: {
       'User-Agent': 'request'
-    };
+    }
   };
 
   request(requestOptions, function(err, response, body) {
     if (err) throw err;
     cb(JSON.parse(response.body));
-  });
+  })
 };
 
 getRepoContributors("jquery", "jquery", function(result) {
   result.forEach(function (avatarBody) {
-    console.log(avatarBody.avatar_url);
+  downloadImageByURL(avatarBody.avatar_url, avatarBody.login)
   })
+
+    function downloadImageByURL(url, filePath) {
+
+    request.get(url) //url
+       .on("error", function (err) {
+          throw err;
+       })
+
+       .on('response', function (response) {
+         console.log('Downloading...', response.statusMessage);
+       })
+
+       .pipe(fs.createWriteStream("./pics/" + filePath + ".jpg"));
+  }
+
 });
+
