df4e953c21188a1d9b4bb245a19c11e7d5fceba1
madebymak <mr.jmak@gmail.com>
2016-11-08 02:49:20 +0000

    Added requestURL link

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,13 @@
 var request = require('request');
 //console.log("Welcome to the GitHub Avatar Downloader!");
-//var GITHUB_USER = "madebymak";
-//var GITHUB_TOKEN = "958480b71aa96b2bd710ee40b90c766a8a9e2a38"
-
-//var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-
-//console.log(requestURL);
+var GITHUB_USER = "madebymak";
+var GITHUB_TOKEN = "958480b71aa96b2bd710ee40b90c766a8a9e2a38"
 
 function getRepoContributors (repoOwner, repoName, cb) {
+
+var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + "@api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+
+console.log(requestURL);
 
 
 }
